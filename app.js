6dceca18a1606bb214a66533f6abf30cf369ca89
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-06-28 18:37:11 +0200

    Add style.css

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
