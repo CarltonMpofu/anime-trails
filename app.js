015a5cf910ead9e5d4227e74240badb9cbefcbe8
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-07-03 18:01:49 +0200

    Finish basic site structure with pictures

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
-});
+}); 
 
 app.listen(3000, function(){
     console.log("Server running on port 3000");
