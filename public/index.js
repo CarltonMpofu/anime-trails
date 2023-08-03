d6c7f49abb824533f7ef6c5919bd7c33ca00fe7d
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-08-03 23:01:36 +0200

    Refactor code. DONE

--- public/index.js
+++ public/index.js
@@ -1,20 +1,12 @@
-// window.alert("Hello");
-
-// <img
-// src="no-photos.png"
-// onerror="this.onerror=null;this.src='https://fallback.url/for/image.jpg'"
-// />
-
-
-
 function changeImageAndLink(myImage, linkID)
 {
+    // Change image source
     myImage.src = "no-photo.png"
     // window.alert(linkID);
     myImage.style.width = "70%"
 
+    // Change image source link information
     const myLinkElement = document.getElementById(linkID);
-
     const newUrl = "https://www.flaticon.com/free-icons/no-photo";
     myLinkElement.innerHTML = "No photo icons created by IconMarketPK - Flaticon";
     myLinkElement.href = newUrl
