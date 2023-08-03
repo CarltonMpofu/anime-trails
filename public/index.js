06384747aaade8377a4af3e39b668404bd4b777b
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-08-03 22:56:56 +0200

    Make image fallback using javascript

--- public/index.js
+++ public/index.js
@@ -4,3 +4,20 @@
 // src="no-photos.png"
 // onerror="this.onerror=null;this.src='https://fallback.url/for/image.jpg'"
 // />
+
+
+
+function changeImageAndLink(myImage, linkID)
+{
+    myImage.src = "no-photo.png"
+    // window.alert(linkID);
+    myImage.style.width = "70%"
+
+    const myLinkElement = document.getElementById(linkID);
+
+    const newUrl = "https://www.flaticon.com/free-icons/no-photo";
+    myLinkElement.innerHTML = "No photo icons created by IconMarketPK - Flaticon";
+    myLinkElement.href = newUrl
+    myLinkElement.title = "no photo icons"
+    
+}
